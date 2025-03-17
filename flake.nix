9332eafe4f2ff3dd7022f704ebff90ee6323f600
Wasit Faraizi <wasit.allin1@gmail.com>
2025-03-17 07:55:20 +0600

    Flatten templates sets to remove `nix flake show` error

--- flake.nix
+++ flake.nix
@@ -9,33 +9,29 @@
           path = ./templates/go;
           description = "Go development environment with go, gopls, git";
         };
-        node = {
-          lts = {
-            path = ./templates/node/lts;
-            description = "Node.js LTS version development environment";
-          };
-          latest = {
-            path = ./templates/node/latest;
-            description = "Node.js Latest version development environment";
-          };
-          "22" = {
-            path = ./templates/node/22;
-            description = "Node.js 22 version development environment";
-          };
-          "20" = {
-            path = ./templates/node/20;
-            description = "Node.js 20 version development environment";
-          };
+        node-lts = {
+          path = ./templates/node/lts;
+          description = "Node.js LTS version development environment";
+        };
+        node-latest = {
+          path = ./templates/node/latest;
+          description = "Node.js Latest version development environment";
+        };
+        "node-22" = {
+          path = ./templates/node/22;
+          description = "Node.js 22 version development environment";
+        };
+        "node-20" = {
+          path = ./templates/node/20;
+          description = "Node.js 20 version development environment";
         };
         python = {
           path = ./templates/python;
           description = "Python dev environment with python3, uv, virtualenv";
         };
-        racket = {
-          sicp = {
-            path = ./templates/sicp;
-            description = "Racket with sicp and tools";
-          };
+        racket-sicp = {
+          path = ./templates/sicp;
+          description = "Racket with sicp and tools";
         };
       };
     };
