dd5066b78df54c8d89df9404a061411cb1488294
Wasit Faraizi <wasit.allin1@gmail.com>
2025-03-22 05:35:28 +0600

    update(node): added configuring steps
    Added pulling github gist for biome config. Added .npmrc creation and
    initialization.

--- templates/node/lts/flake.nix
+++ templates/node/lts/flake.nix
@@ -17,6 +17,17 @@
           biome
           git
         ];
+        shellHook = ''
+          echo "Configuring biome ..."
+          touch biome.jsonc
+          curl "https://gist.githubusercontent.com/wasituf/4a449cfa15293cc385cdc94579d1d461/raw/1992e4522ccbc72f93715ec18f10fc3115f05a33/biome.jsonc" biome.jsonc
+          echo "Configured biome."
+
+          echo "Configuring .npmrc ..."
+          touch .npmrc
+          echo "auto-install-peers=true\nnode-linker=hoisted\nlockfile=true" > .npmrc
+          echo "Configured .npmrc"
+        '';
       };
     };
 }
