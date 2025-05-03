6986af7923e38d63e0054263b9d251df1859076e
wasituf <wasit.allin1@proton.me>
2025-05-04 01:07:38 +0600

    extend go flake with package definition

--- templates/go/flake.nix
+++ templates/go/flake.nix
@@ -3,20 +3,54 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      gomod2nix,
+    }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ gomod2nix.overlays.default ];
+      };
+      goPackage = pkgs.buildGoApplication {
+        pname = "app-name";
+        version = "0.1.0";
+        src = ./.;
+        modules = ./gomod2nix.toml;
+        nativeBuildInputs = with pkgs; [
+          # pkg-config
+        ];
+        buildInputs = with pkgs; [
+          # gtk3
+          # glib
+          # cairo
+        ];
+      };
     in
     {
+      packages.${system}.default = goPackage;
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs; [
           git
           go
           gopls
           gotestsum
+          gomod2nix.packages.${system}.default
+
+          # gtk3
+          # glib
+          # cairo
+          # pkg-config
         ];
       };
     };
