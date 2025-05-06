9d689f82c602a8cf7f90be585479a73c8c032673
wasituf <wasit.allin1@proton.me>
2025-05-06 20:22:52 +0600

    add corepack to node flakes

--- templates/node/latest/flake.nix
+++ templates/node/latest/flake.nix
@@ -14,6 +14,7 @@
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs; [
           nodePackages_latest.nodejs
+          corepack_latest
           biome
           git
         ];
